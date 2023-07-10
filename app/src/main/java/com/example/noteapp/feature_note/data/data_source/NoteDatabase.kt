ac9e343af5147cbe55247479f5430cec6244199e
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2023-07-10 20:59:05 +0800

    hilt

--- app/src/main/java/com/example/noteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/example/noteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -10,4 +10,8 @@ import com.example.noteapp.feature_note.domain.model.Note
 )
 abstract class NoteDatabase: RoomDatabase() {
     abstract val noteDao: Dao
+
+    companion object {
+        const val DATABASE_NAME = "notes_database"
+    }
 }
