ce6d189fc6bcedb4aa524f981f0b7f7242cf3b87
Deadboiklek <91292342+Deadboiklek@users.noreply.github.com>
2023-07-22 23:24:44 +0800

    AddEditViewModel

--- app/src/main/java/com/example/noteapp/di/AppModule.kt
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -8,6 +8,7 @@ import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.noteapp.feature_note.domain.repository.NoteRepository
 import com.example.noteapp.feature_note.domain.use_case.AddNote
 import com.example.noteapp.feature_note.domain.use_case.DeleteNote
+import com.example.noteapp.feature_note.domain.use_case.GetNote
 import com.example.noteapp.feature_note.domain.use_case.GetNotes
 import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -42,7 +43,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
